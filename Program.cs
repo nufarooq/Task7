318fd3fba15fecb76a6e886a906c4792c79fde29
Tejora <numan.992@gmail.com>
2023-01-20 19:59:28 +0530

    Task commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Lowercase2Uppercase
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputFileName = @"D:\InductionTasks\Lowercase2Uppercase\lowercasefile.txt";
+            string outputFileName = @"D:\InductionTasks\Lowercase2Uppercase\uppercasefile.txt";
+
+            string contents = File.ReadAllText(inputFileName);
+            contents = contents.ToUpper();
+
+            File.WriteAllText(outputFileName, contents);
+        }
+    }
+}
